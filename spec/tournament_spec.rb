b5925e9b2fbe5d959d70e0e7da703d6822f4e6dc
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2024-04-26 13:01:32 +0100

    TEST: RED - a win is worth 3 points

--- spec/tournament_spec.rb
+++ spec/tournament_spec.rb
@@ -24,4 +24,21 @@ RSpec.describe Tournament do
       expect(tournament.tally(input)).to eq(expected)
     end
   end
+
+  describe 'Tournaments are recorded correctly' do
+    tournament = described_class.new
+    # rubocop:disable RSpec/ExampleLength
+    it 'wins are worth 3 points' do 
+      input = <<~INPUT
+        Allegoric Alaskans;Blithering Badgers;win
+      INPUT
+      expected = <<~TALLY
+        Team                           | MP |  W |  D |  L |  P\n
+        Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3\n
+        Blithering Badgers             |  1 |  0 |  0 |  1 |  3\n
+      TALLY
+      expect(tournament.tally(input)).to eq(expected)
+    end
+
+  end
 end
